e95af8e4eb11d3102be34d18d8680d04ce570031
andrewcgaitskell <andrew@gaitskell.com>
2025-10-11 10:27:16 +0100

    Uncomment main execution block for app

--- blockly/main.py
+++ blockly/main.py
@@ -41,5 +41,5 @@ async def run_code():
     except Exception as e:
         return jsonify({"output": str(e)}), 400
 
-#if __name__ == "__main__":
-#    app.run(host="0.0.0.0", port=5001)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5001)
