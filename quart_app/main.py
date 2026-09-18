b0a8f5b9fcb5f0556f0d5ae16e1eaf29dbc39c5e
andrewcgaitskell <andrew@gaitskell.com>
2026-09-18 18:04:15 +0100

    Update main.py

--- quart_app/main.py
+++ quart_app/main.py
@@ -143,5 +143,5 @@ async def index():
 
 if __name__ == "__main__":
     # No HASS_TOKEN check here; blueprint will raise a helpful error if missing when called.
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="127.0.0.1", port=5000)
 
